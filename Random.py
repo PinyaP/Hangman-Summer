efbf18a65b5ea7122dadd89eb5b585b8a542a176
sakchaiv <78199153+sakchaiv@users.noreply.github.com>
2022-06-17 18:51:43 +0700

    Add files via upload

--- Random.py
+++ Random.py
@@ -1,26 +1,38 @@
+
 import random
 
 category = ["animal.txt", "food.txt", "fruit.txt", "occupation.txt", "place.txt"]
 fileW = open(random.choice(category),'r')
 
 
-level = {"NORMAL|50":5, "HARD|150":4, "LEGENDAR|200":2}
+level = {"NORMAL|50":1, "HARD|150":7, "LEGENDAR|200":12}
+level_list = list(level)
 randomLv = random.choice(list(level))
 
-#print(randomLv, line)
 
-#print(level["NORMAL"])
+
+indexlevel = level_list.index(randomLv)
+if indexlevel < len(level_list) - 1:
+    indexlevel += 1
+    index_stop = level[level_list[indexlevel]]
+else:
+    index_stop = 13
+
+index = random.randint(level[randomLv] ,index_stop)
+print(index)
+if index in [0,6,11]:
+    index +=1 
 
 wordlist = fileW.readlines()
-for word in wordlist:
-    if word == randomLv:
-        print(word)
+print(wordlist[index])
+    
+    
+        
 
 
 
 
 #print(wordlist)
-
 
 
 
